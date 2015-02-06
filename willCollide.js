dfa130f350f77a5b509eb1f18f5a5f7c32bfc155
Jim Veneskey <github@strifecrafter.com>
2015-02-05 20:36:20 -0500

    Implemented rudimentary collision avoidance routine

---
+++ willCollide.js
@@ -0,0 +1,44 @@
+function willCollide(Ax, Ay, Bx, By, boundBoxX, boundBoxY) {
+	// For collision detection (avoidance) we check to see if either
+	// Ax,Ay or Bx,By is within the player tank bounding box
+	var collideX = false;
+	var collideY = false;
+	var imminent = false;
+	// check Ax and Bx separately for simplicity
+	if (Ax >= boundBoxX && Ax <= boundBoxX + 40) {
+		console.log("Possible Collision on the X detected by Ax");
+		console.log("Ax, boundBoxX: " + Ax + " , " + boundBoxX);
+		collideX = true;
+	} else {
+		console.log("No collision on the X detected by Ax");
+	}
+
+	if (Bx >= boundBoxX && Bx <= boundBoxX + 40) {
+		console.log("Possible Collision on the X detected by Bx");
+		console.log("Bx, boundBoxX: " + Bx + " , " + boundBoxX);
+		collideX = true;
+	} else {
+		console.log("No collision on the X detected by Bx");
+	}
+
+	if (Ay >= boundBoxY && Ay <= boundBoxY + 50) {
+		console.log("Possible Collision on the Y detected by Ay");
+		console.log("Ay, boundBoxY: " + Ay + " , " + boundBoxY);
+		collideY = true;
+	} else {
+		console.log("No collision on the Y detected by Ay");
+	}
+
+	if (By >= boundBoxY && By <= boundBoxY + 50) {
+		console.log("Possible Collision on the Y detected by By");
+		console.log("By, boundBoxY: " + By + " , " + boundBoxY);
+		collideY = true;
+	} else {
+		console.log("No collision on the Y detected by By");
+	}
+
+	if (collideX && collideY) {
+		imminent = true;
+	}
+	return imminent;
+}
