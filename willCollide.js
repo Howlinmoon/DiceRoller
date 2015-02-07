ec1e087c9a2aa00b066b5b7c677d3cb61fd511aa
Jim Veneskey <github@strifecrafter.com>
2015-02-07 11:11:28 -0500

    Working on intelligently turning away from wall collisions and stopping while collision event is active

--- willCollide.js
+++ willCollide.js
@@ -16,34 +16,26 @@ function willCollide(Ax, Ay, Bx, By, boundBoxX, boundBoxY) {
 		console.log("Possible Collision on the X detected by Ax");
 		console.log("Ax, boundBoxX: " + Ax + " , " + boundBoxX);
 		AcollideX = true;
-	} else {
-		console.log("No collision on the X detected by Ax");
 	}
-
+	
 	if (Bx >= boundBoxX && Bx <= boundBoxX + 75) {
 		console.log("Possible Collision on the X detected by Bx");
 		console.log("Bx, boundBoxX: " + Bx + " , " + boundBoxX);
 		BcollideX = true;
-	} else {
-		console.log("No collision on the X detected by Bx");
 	}
-
+	
 	if (Ay >= boundBoxY && Ay <= boundBoxY + 75) {
 		console.log("Possible Collision on the Y detected by Ay");
 		console.log("Ay, boundBoxY: " + Ay + " , " + boundBoxY);
 		AcollideY = true;
-	} else {
-		console.log("No collision on the Y detected by Ay");
 	}
-
+	
 	if (By >= boundBoxY && By <= boundBoxY + 75) {
 		console.log("Possible Collision on the Y detected by By");
 		console.log("By, boundBoxY: " + By + " , " + boundBoxY);
 		BcollideY = true;
-	} else {
-		console.log("No collision on the Y detected by By");
 	}
-
+	
 	// Now that we have gotten our collision votes - we examine them to see if we have an actual
 	// collision or just a false alarm
 	// init our overall results
